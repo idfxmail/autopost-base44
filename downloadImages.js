5102f269071d2338dc341a103278ac1724dfa482
idfxmail <idfxmail@gmail.com>
2025-08-10 20:13:37 +0400

    Delete downloadImages.js

--- downloadImages.js
+++
@@ -1,6 +0,0 @@
-
-const fs=require('fs-extra');const path=require('path');const fetch=require('node-fetch');
-async function downloadImages(urls=[],tmpDir='./tmp'){await fs.ensureDir(tmpDir);const files=[];
-for(let i=0;i<urls.length;i++){const u=urls[i];const ext=(u.split('?')[0].split('.').pop()||'jpg').toLowerCase();
-const p=path.join(tmpDir,`img_${Date.now()}_${i}.${ext}`);const res=await fetch(u);if(!res.ok)throw new Error('Failed '+u);
-const buf=await res.buffer();await fs.writeFile(p,buf);files.push(p);}return files;} module.exports={downloadImages};
